4719551c09ac12ad6570a18f0d69a2948e8217b6
Fiza <fizaasyed03@gmail.com>
2025-07-03 22:05:26 +0530

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -7,6 +7,13 @@ sudo snap install alacritty --classic
 
 sudo apt install fonts-firacode
 
+
+sudo apt install nodejs npm
+sudo npm install -g typescript typescript-language-server
+sudo apt install clangd
+sudo npm install -g pyright
+
+
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/nvim
 
@@ -14,8 +21,11 @@ cp -r ~/fin/alacritty/* ~/.config/alacritty/
 cp -r ~/fin/nvim/* ~/.config/nvim/
 cp ~/fin/.tmux.conf ~/
 tmux source-file ~/.tmux.conf
+
 git clone --depth 1 https://github.com/wbthomason/packer.nvim \
 ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+git clone https://github.com/folke/lazy.nvim ~/.local/share/nvim/lazy/lazy.nvim
 
 
 echo "All configs copied successfully!"
